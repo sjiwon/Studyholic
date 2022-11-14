e522222f448cdf57506c08d96225b2fb2250ab88
SeoJiWon <sjiwon4491@gmail.com>
2022-11-14 23:44:06 +0900

    add: @ExceptionHandler ApiExceptionHandler assignableTypes 추가

--- src/main/java/com/sjiwon/studyholic/web/api/ApiExceptionHandler.java
+++ src/main/java/com/sjiwon/studyholic/web/api/ApiExceptionHandler.java
@@ -4,12 +4,16 @@ import com.sjiwon.studyholic.exception.ErrorResponse;
 import com.sjiwon.studyholic.exception.StudyholicErrorCode;
 import com.sjiwon.studyholic.exception.StudyholicException;
 import com.sjiwon.studyholic.web.api.authenticate.AuthenticationApiController;
+import com.sjiwon.studyholic.web.api.study.StudyApiController;
 import com.sjiwon.studyholic.web.api.user.UserApiController;
+import com.sjiwon.studyholic.web.api.userstudy.UserStudyApiController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(assignableTypes = {AuthenticationApiController.class, UserApiController.class})
+@RestControllerAdvice(assignableTypes = {
+        AuthenticationApiController.class, UserApiController.class, StudyApiController.class, UserStudyApiController.class
+})
 public class ApiExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> catchAnotherArtException(StudyholicException ex) {
