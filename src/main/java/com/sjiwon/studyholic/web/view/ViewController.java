e9b3d64ba8efe0448bad49f5897af7a1424493b6
SeoJiWon <sjiwon4491@gmail.com>
2022-11-14 09:21:21 +0900

    feat : ViewController 로그인 페이지 추가 (#4)

---
+++ src/main/java/com/sjiwon/studyholic/web/view/ViewController.java
@@ -0,0 +1,14 @@
+package com.sjiwon.studyholic.web.view;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ViewController {
+    @GetMapping("/login")
+    public String loginPage() {
+        return "authenticate/LoginPage";
+    }
+}
