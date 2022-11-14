26e9189f383498759020c85e9c386c72c1f1947c
SeoJiWon <sjiwon4491@gmail.com>
2022-11-14 11:21:45 +0900

    feat: 파일 업로드 Service 구현 (#5)

---
+++ src/main/java/com/sjiwon/studyholic/domain/file/dto/UploadProfile.java
@@ -0,0 +1,11 @@
+package com.sjiwon.studyholic.domain.file.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UploadProfile {
+    private String uploadName;
+    private String storageName;
+}
