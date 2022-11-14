ac328a43c99ba52041cde38ecacb469dead821a3
SeoJiWon <sjiwon4491@gmail.com>
2022-11-14 16:42:11 +0900

    feat: 스터디 수정 API (#17)

--- src/main/java/com/sjiwon/studyholic/web/api/study/StudyApiController.java
+++ src/main/java/com/sjiwon/studyholic/web/api/study/StudyApiController.java
@@ -2,15 +2,14 @@ package com.sjiwon.studyholic.web.api.study;
 
 import com.sjiwon.studyholic.domain.entity.study.service.StudyService;
 import com.sjiwon.studyholic.web.api.study.dto.request.StudyCreateRequest;
+import com.sjiwon.studyholic.web.api.study.dto.request.UpdateStudyInformationRequest;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 
 @RestController
@@ -25,5 +24,12 @@ public class StudyApiController {
     public ResponseEntity<Void> createStudy(@RequestBody StudyCreateRequest request) {
         Long createStudy = studyService.createNewStudy(request.getUserId(), request.toEntity(), request.getTagList());
         return ResponseEntity.created(URI.create("/study/" + createStudy)).build();
+    }
+
+    @PatchMapping("/study/{studyId}")
+    @ApiOperation(value = "스터디 정보 수정 API", notes = "스터디의 정보를 수정하는 API [스터디 팀명, 간단한 설명, 설명, 모집 마감일, 최대 모집 멤버]")
+    public ResponseEntity<Void> changeStudyInformation(@PathVariable Long studyId, @RequestBody UpdateStudyInformationRequest updateRequest, HttpServletRequest request) {
+        studyService.changeInformation(request, studyId, updateRequest.getName(), updateRequest.getBriefDescription(), updateRequest.getDescription(), updateRequest.getRecruitDeadline(), updateRequest.getMaxMember());
+        return ResponseEntity.noContent().build();
     }
 }
