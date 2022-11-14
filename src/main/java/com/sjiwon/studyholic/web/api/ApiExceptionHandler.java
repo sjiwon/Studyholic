0e194c4dcd484523ac2e94a5da18fc1b2e9296fe
SeoJiWon <sjiwon4491@gmail.com>
2022-11-14 09:39:36 +0900

    feat: ApiExceptionHandler @ExceptionHandler 추가 (#4)

---
+++ src/main/java/com/sjiwon/studyholic/web/api/ApiExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.sjiwon.studyholic.web.api;
+
+import com.sjiwon.studyholic.exception.ErrorResponse;
+import com.sjiwon.studyholic.exception.StudyholicErrorCode;
+import com.sjiwon.studyholic.exception.StudyholicException;
+import com.sjiwon.studyholic.web.api.authenticate.AuthenticationApiController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(assignableTypes = {AuthenticationApiController.class})
+public class ApiExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> catchAnotherArtException(StudyholicException ex) {
+        StudyholicErrorCode errorCode = ex.getErrorCode();
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(ErrorResponse.of(errorCode));
+    }
+}
