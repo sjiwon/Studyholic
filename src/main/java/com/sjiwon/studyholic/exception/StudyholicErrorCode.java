13da6a6c61713d9fa1a18de58741848a51bf76f1
SeoJiWon <sjiwon4491@gmail.com>
2022-11-14 10:24:10 +0900

    add: 회원가입 중복체크 관련 Exception Enum 추가 (#5)

--- src/main/java/com/sjiwon/studyholic/exception/StudyholicErrorCode.java
+++ src/main/java/com/sjiwon/studyholic/exception/StudyholicErrorCode.java
@@ -8,6 +8,8 @@ public enum StudyholicErrorCode {
     // User
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 정보가 존재하지 않습니다"),
     WRONG_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다"),
+    DUPLICATE_USER_NICKNAME(HttpStatus.CONFLICT, "중복된 닉네임입니다"),
+    DUPLICATE_USER_LOGIN_ID(HttpStatus.CONFLICT, "중복된 로그인 아이디입니다"),
 
     ;
 
