5416f3dbe4bd1fc9f5b2567657d0a5561e7f8b85
SeoJiWon <sjiwon4491@gmail.com>
2022-11-17 09:42:01 +0900

    refactor: AbstractUserPrincipal 스펙 변경 (#26)

--- src/main/java/com/sjiwon/studyholic/domain/entity/AbstractUserPrincipal.java
+++ src/main/java/com/sjiwon/studyholic/domain/entity/AbstractUserPrincipal.java
@@ -12,15 +12,21 @@ import lombok.NoArgsConstructor;
 public class AbstractUserPrincipal {
     private Long id;
     private String name;
+    private String nickname;
     private String loginId;
     private String password;
+    private String email;
+    private String storageName;
     private String role;
 
     public AbstractUserPrincipal(User user) {
         this.id = user.getId();
         this.name = user.getName();
+        this.nickname = user.getNickName();
         this.loginId = user.getLoginId();
         this.password = user.getLoginPassword();
+        this.email = user.getEmail();
+        this.storageName = user.getStorageName();
         this.role = user.getRole().getAuthority();
     }
 }
