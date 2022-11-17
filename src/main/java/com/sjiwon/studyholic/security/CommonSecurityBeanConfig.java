c17b753b96c0b51d60c3ec2d061935ea462be1ce
SeoJiWon <sjiwon4491@gmail.com>
2022-11-17 15:06:41 +0900

    add: Security 관련 공통 Bean 설정 (#26)

---
+++ src/main/java/com/sjiwon/studyholic/security/CommonSecurityBeanConfig.java
@@ -0,0 +1,26 @@
+package com.sjiwon.studyholic.security;
+
+import com.sjiwon.studyholic.domain.entity.user.repository.UserRepository;
+import com.sjiwon.studyholic.security.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class CommonSecurityBeanConfig {
+    private final UserRepository userRepository;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService(userRepository);
+    }
+}
