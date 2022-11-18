0dc7177dd97f581eb6e5cab85b15dd528225a0f6
SeoJiWon <sjiwon4491@gmail.com>
2022-11-18 16:00:37 +0900

    feat: 별도의 세션에 사용자의 닉네임, 이미지 서버 저장명 제공 기능 구현 (#35)

--- src/main/java/com/sjiwon/studyholic/security/authentication/AjaxAuthenticationSuccessHandler.java
+++ src/main/java/com/sjiwon/studyholic/security/authentication/AjaxAuthenticationSuccessHandler.java
@@ -1,6 +1,9 @@
 package com.sjiwon.studyholic.security.authentication;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sjiwon.studyholic.domain.etc.session.dto.UserSessionDto;
+import com.sjiwon.studyholic.security.principal.UserAuthenticationDto;
+import com.sjiwon.studyholic.security.principal.UserPrincipal;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,8 +15,11 @@ import org.springframework.security.web.savedrequest.SavedRequest;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Objects;
+
+import static com.sjiwon.studyholic.common.VariableFactory.SESSION_KEY;
 
 public class AjaxAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
     private final ObjectMapper objectMapper = new ObjectMapper();
@@ -23,6 +29,9 @@ public class AjaxAuthenticationSuccessHandler implements AuthenticationSuccessHa
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        // 별도의 세션에 사용자의 [닉네임, 이미지 서버 저장명] 제공
+        generateSpecialStorageSession(request, authentication);
+        
         response.setStatus(HttpStatus.OK.value());
         response.setCharacterEncoding("UTF-8");
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
@@ -39,5 +48,11 @@ public class AjaxAuthenticationSuccessHandler implements AuthenticationSuccessHa
         }
 
         objectMapper.writeValue(response.getWriter(), redirectUrl);
+    }
+
+    private void generateSpecialStorageSession(HttpServletRequest request, Authentication authentication) {
+        HttpSession session = request.getSession();
+        UserAuthenticationDto user = ((UserPrincipal) authentication.getPrincipal()).getUser();
+        session.setAttribute(SESSION_KEY, new UserSessionDto(user.getNickname(), user.getStorageName()));
     }
 }
