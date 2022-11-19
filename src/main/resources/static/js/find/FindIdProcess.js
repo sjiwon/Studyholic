f3e3cd016392f2eff5e7b247931b6fdd3a6f7841
SeoJiWon <sjiwon4491@gmail.com>
2022-11-19 17:15:37 +0900

    refactor: 아이디 찾기 관련 alert -> SweetAlert 수정 (#34)

--- src/main/resources/static/js/find/FindIdProcess.js
+++ src/main/resources/static/js/find/FindIdProcess.js
@@ -1,37 +1,82 @@
 function findIdProcess() {
+    const ToastResponse = Swal.mixin({
+        focusConfirm: false,
+        returnFocus: false
+    });
+
     let name = $('#name');
     let nameToken = $('#nameToken');
     if (validationName(name, nameToken) === false) {
+        ToastResponse.fire({
+            showConfirmButton: false,
+            timer: 1000,
+            timerProgressBar: true,
+            text: '이름을 다시 확인해주세요',
+            icon: 'warning'
+        }).then(() => {
+            name.focus();
+        })
         return false;
     }
 
     let email = $('#email');
     let emailVerificationToken = $('#emailVerificationToken');
-    if (validationEmail(email, emailVerificationToken) === false) {
+    if (validationEmail(email, emailVerificationToken) === 'fail1') {
+        ToastResponse.fire({
+            showConfirmButton: false,
+            timer: 1000,
+            timerProgressBar: true,
+            text: '이메일을 다시 확인해주세요',
+            icon: 'warning'
+        }).then(() => {
+            email.focus();
+        })
+        return false;
+    } else if (validationEmail(email, emailVerificationToken) === 'fail2') {
+        ToastResponse.fire({
+            showConfirmButton: false,
+            timer: 1000,
+            timerProgressBar: true,
+            text: '이메일 인증을 진행해주세요',
+            icon: 'warning'
+        }).then(() => {
+            emailVerificationToken.focus();
+        })
         return false;
     }
 
     let data = {
         'name': name.val(),
         'email': email.val()
-    }
+    };
 
     axios.post('/api/user/find/login-id', data)
-        .then((response) => {
-            alert('아이디는 [' + response['data'] + ']입니다.\n로그인 페이지로 이동합니다');
-            location.href = '/login';
+        .then((res) => {
+            ToastResponse.fire({
+                html: '아이디는 <b>' + res['data'] + '</b>입니다.<br>로그인 페이지로 이동합니다',
+                icon: 'success'
+            }).then(() => {
+                location.href = '/login';
+            })
         })
         .catch(error => {
             let jsonData = error.response.data;
-            alert(jsonData['message']);
-            location.replace('/find-id');
+
+            ToastResponse.fire({
+                showConfirmButton: false,
+                timer: 1000,
+                timerProgressBar: true,
+                color: '#FF0000',
+                text: jsonData['message'],
+                icon: 'error'
+            }).then(() => {
+                location.replace('/find-id');
+            });
         });
 }
 
 function validationName(name, nameToken) {
     if (nameToken.val() === 'fail') {
-        alert('이름을 다시 확인해주세요');
-        name.focus();
         return false;
     }
 }
@@ -39,13 +84,9 @@ function validationName(name, nameToken) {
 function validationEmail(email, emailToken) {
     if (emailToken.val() === 'fail') {
         if (email.val().trim() === '') {
-            alert('이메일을 다시 확인해주세요');
-            email.focus();
-            return false;
+            return 'fail1';
         } else {
-            alert('이메일 인증을 진행해주세요');
-            emailToken.focus();
-            return false;
+            return 'fail2';
         }
     }
 }
