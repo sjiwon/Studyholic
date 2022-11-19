2081e43d5e1f7a75202857e53ce0179f98387f5f
SeoJiWon <sjiwon4491@gmail.com>
2022-11-19 16:05:46 +0900

    refactor: Logout.js alert -> SweetAlert 수정 (#34)

--- src/main/resources/static/js/login/Logout.js
+++ src/main/resources/static/js/login/Logout.js
@@ -1,12 +1,31 @@
 function logout() {
-    let process = confirm("로그아웃 하시겠습니까?");
-    if (process) {
-        axios.post('/api/logout')
-            .then(() => {
-                alert('로그아웃이 완료되었습니다');
-                location.replace('/');
-            });
-    } else {
-        return false;
-    }
+    const ToastApi = Swal.mixin({
+        confirmButtonText: '네',
+        showCancelButton: true,
+        cancelButtonText: '아니요',
+        focusConfirm: false
+    });
+
+    const ToastResponse = Swal.mixin({
+        showConfirmButton: false,
+        timer: 1000,
+        timerProgressBar: true
+    });
+
+    ToastApi.fire({
+        text: '로그아웃 하시겠습니까?',
+        icon: 'question',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            axios.post('/api/logout')
+                .then(() => {
+                    ToastResponse.fire({
+                        text: '로그아웃이 완료되었습니다',
+                        icon: 'success'
+                    }).then(() => {
+                        location.replace('/');
+                    })
+                });
+        }
+    });
 }
