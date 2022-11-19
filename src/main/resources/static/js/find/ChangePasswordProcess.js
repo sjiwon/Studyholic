5718f6d3921752a074d1150be46b0bab0e92254e
SeoJiWon <sjiwon4491@gmail.com>
2022-11-19 19:13:36 +0900

    refactor: 비밀번호 재설정 관련 alert -> SweetAlert 수정 (#34)

--- src/main/resources/static/js/find/ChangePasswordProcess.js
+++ src/main/resources/static/js/find/ChangePasswordProcess.js
@@ -1,82 +1,146 @@
 function changePassword(userId) {
+    const ToastResponse = Swal.mixin({
+        focusConfirm: false,
+        returnFocus: false
+    });
+
     let currentPassword = $('#currentPassword');
     let currentPasswordToken = $('#currentPasswordToken');
     if (validationCurrentPassword(currentPassword, currentPasswordToken) === false) {
+        ToastResponse.fire({
+            showConfirmButton: false,
+            timer: 1000,
+            timerProgressBar: true,
+            text: '현재 비밀번호를 다시 확인해주세요',
+            icon: 'warning'
+        }).then(() => {
+            currentPassword.focus();
+        })
         return false;
     }
 
     let changePassword = $('#changePassword');
     let changePasswordToken = $('#changePasswordToken');
     if (validationChangePassword(changePassword, changePasswordToken) === false) {
+        ToastResponse.fire({
+            showConfirmButton: false,
+            timer: 1000,
+            timerProgressBar: true,
+            text: '변경할 비밀번호를 다시 확인해주세요',
+            icon: 'warning'
+        }).then(() => {
+            $('#checkChangePassword').val('');
+            changePassword.focus();
+        })
         return false;
     }
 
     let checkChangePassword = $('#checkChangePassword');
     let checkChangePasswordVerificationToken = $('#checkChangePasswordVerificationToken');
     if (validationChangePasswordVerification(checkChangePassword, checkChangePasswordVerificationToken) === false) {
+        ToastResponse.fire({
+            showConfirmButton: false,
+            timer: 1000,
+            timerProgressBar: true,
+            text: '변경할 비밀번호 확인란을 다시 확인해주세요',
+            icon: 'warning'
+        }).then(() => {
+            checkChangePassword.focus();
+        })
         return false;
     }
 
     if (validateionChangePasswordAndVericifactionInput(changePassword, checkChangePassword) === false) {
+        ToastResponse.fire({
+            showConfirmButton: false,
+            timer: 1000,
+            timerProgressBar: true,
+            text: '변경할 비밀번호와 확인란이 일치하지 않습니다',
+            icon: 'warning'
+        }).then(() => {
+            checkChangePassword.focus();
+        })
         return false;
     }
 
-    let select = confirm('비밀번호를 변경하시겠습니까?');
-    if (select) {
-        let data = {
-            'userId': userId,
-            'currentPassword': currentPassword.val(),
-            'changePassword': changePassword.val()
-        };
+    const ToastApi = Swal.mixin({
+        confirmButtonText: '네',
+        showCancelButton: true,
+        cancelButtonText: '아니요',
+        focusConfirm: false
+    });
 
-        axios.patch('/api/user/change-password', data)
-            .then(() => {
-                axios.post("/api/logout")
-                    .then(() => {
-                        alert('비밀번호가 변경되었습니다\n다시 로그인해주세요');
-                        location.href = '/login';
+    ToastApi.fire({
+        text: '비밀번호를 변경하시겠습니까?',
+        icon: 'question'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            let data = {
+                'userId': userId,
+                'currentPassword': currentPassword.val(),
+                'changePassword': changePassword.val()
+            };
+
+            axios.patch('/api/user/change-password', data)
+                .then(() => {
+                    axios.post("/api/logout")
+                        .then(() => {
+                            ToastResponse.fire({
+                                html: '비밀번호가 변경되었습니다<br>다시 로그인해주세요',
+                                icon: 'success'
+                            }).then(() => {
+                                location.href = '/login';
+                            })
+                        })
+                        .catch(error => {
+                            let jsonData = error.response.data;
+
+                            ToastResponse.fire({
+                                showConfirmButton: false,
+                                timer: 1000,
+                                timerProgressBar: true,
+                                color: '#FF0000',
+                                text: jsonData['message'],
+                                icon: 'error'
+                            })
+                        });
+                })
+                .catch(error => {
+                    let jsonData = error.response.data;
+
+                    ToastResponse.fire({
+                        showConfirmButton: false,
+                        timer: 1000,
+                        timerProgressBar: true,
+                        color: '#FF0000',
+                        text: jsonData['message'],
+                        icon: 'error'
                     })
-                    .catch(error => {
-                        let jsonData = error.response.data;
-                        alert(jsonData['message']);
-                    });
-            })
-            .catch(error => {
-                let jsonData = error.response.data;
-                alert(jsonData['message']);
-            });
-    } else {
-        return false;
-    }
+                });
+        }
+    })
 }
 
 function validationCurrentPassword(password, passwordToken) {
     if (passwordToken.val() === 'fail') {
-        alert('현재 비밀번호를 다시 확인해주세요');
-        password.focus();
         return false;
     }
 }
 
 function validationChangePassword(password, passwordToken) {
     if (passwordToken.val() === 'fail') {
-        alert('변경할 비밀번호를 다시 확인해주세요');
-        password.focus();
         return false;
     }
 }
 
 function validationChangePasswordVerification(password, passwordToken) {
     if (passwordToken.val() === 'fail') {
-        alert('변경할 비밀번호 확인란을 다시 확인해주세요');
-        password.focus();
         return false;
     }
 }
 
 function validateionChangePasswordAndVericifactionInput(password, checkPassword) {
     if (password.val() !== checkPassword.val()) {
-        alert('비밀번호와 비밀번호 확인란이 일치하지 않습니다.\n다시 확인해주세요');
         return false;
     }
 }
