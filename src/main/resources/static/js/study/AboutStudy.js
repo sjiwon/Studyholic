f4e72d53ace568a2e8f11edd613882a50b4632af
SeoJiWon <sjiwon4491@gmail.com>
2022-11-21 20:13:34 +0900

    refactor: AboutStudy.js 국제화 메시지 설정 추가 (#45)

--- src/main/resources/static/js/study/AboutStudy.js
+++ src/main/resources/static/js/study/AboutStudy.js
@@ -17,8 +17,12 @@ function participate(studyName, studyId, userId) {
         focusConfirm: false
     });
 
+    let question = (navigator.language === 'ko')
+        ? ('<b>' + studyName + '</b>에 참여하시겠습니까?')
+        : ('Would you like to join <b>' + studyName + '?</b>');
+
     ToastApi.fire({
-        html: '<b>[' + studyName + ']</b>에 참여하시겠습니까?',
+        html: question,
         icon: 'question'
     }).then((result) => {
         if (result.isConfirmed) {
@@ -29,8 +33,12 @@ function participate(studyName, studyId, userId) {
 
             axios.post('/api/user/join-study', data)
                 .then(() => {
+                    let successHtml = (navigator.language === 'ko')
+                        ? ('<b>' + studyName + '</b> 참여가 완료되었습니다')
+                        : ('<b>' + studyName + '</b> Engagement is complete');
+
                     ToastResponse.fire({
-                        html: '<b>[' + studyName + ']</b> 참여가 완료되었습니다',
+                        html: successHtml,
                         icon: 'success'
                     }).then(() => {
                         location.href = '/user/' + userId + '/study';
@@ -38,11 +46,35 @@ function participate(studyName, studyId, userId) {
                 })
                 .catch((error) => {
                     let jsonData = error.response.data;
+
+                    let errorText;
+                    if (jsonData['message'] === '스터디 정보가 존재하지 않습니다') {
+                        errorText = (navigator.language === 'ko')
+                            ? (jsonData['message'])
+                            : ('Study information does not exist');
+                    } else if (jsonData['message'] === '모집 완료된 스터디입니다') {
+                        errorText = (navigator.language === 'ko')
+                            ? (jsonData['message'])
+                            : ('This is a completed study');
+                    } else if (jsonData['message'] === '사용자 정보가 존재하지 않습니다') {
+                        errorText = (navigator.language === 'ko')
+                            ? (jsonData['message'])
+                            : ('User information does not exist');
+                    } else if (jsonData['message'] === '이미 참여하고 있는 스터디입니다') {
+                        errorText = (navigator.language === 'ko')
+                            ? (jsonData['message'])
+                            : ('This is a study you are already participating in.');
+                    } else {
+                        errorText = (navigator.language === 'ko')
+                            ? (jsonData['message'])
+                            : ('An error occurred on the server while sending the email');
+                    }
+
                     ToastResponse.fire({
                         color: '#FF0000',
-                        text: jsonData['message'],
+                        text: errorText,
                         icon: 'error'
-                    })
+                    });
                 });
         }
     });
@@ -62,8 +94,12 @@ function participateCancle(studyName, studyId, userId) {
         focusConfirm: false
     });
 
+    let question = (navigator.language === 'ko')
+        ? ('<b>' + studyName + '</b> 참여를 취소하시겠습니까?')
+        : ('Are you sure you want to cancel your participation in the <b>' + studyName + '?</b>');
+
     ToastApi.fire({
-        html: '<b>[' + studyName + ']</b> 참여를 취소하시겠습니까?',
+        html: question,
         icon: 'question'
     }).then((result) => {
         if (result.isConfirmed) {
@@ -74,8 +110,12 @@ function participateCancle(studyName, studyId, userId) {
 
             axios.post('/api/user/cancel-study', data)
                 .then(() => {
+                    let successHtml = (navigator.language === 'ko')
+                        ? ('<b>' + studyName + '</b> 참여 취소가 완료되었습니다')
+                        : ('<b>' + studyName + '</b> participation cancellation has been completed');
+
                     ToastResponse.fire({
-                        html: '<b>[' + studyName + ']</b> 참여 취소가 완료되었습니다',
+                        html: successHtml,
                         icon: 'success'
                     }).then(() => {
                         location.href = '/user/' + userId + '/study';
@@ -107,15 +147,23 @@ function deleteStudy(studyName, studyId) {
         focusConfirm: false
     });
 
+    let question = (navigator.language === 'ko')
+        ? ('<b>' + studyName + '</b>를 삭제하시겠습니까?')
+        : ('Are you sure you want to delete <b>' + studyName + '?</b>');
+
     ToastApi.fire({
-        html: '<b>[' + studyName + ']</b>를 삭제하시겠습니까?',
+        html: question,
         icon: 'question'
     }).then((result) => {
         if (result.isConfirmed) {
             axios.delete('/api/study/' + studyId)
                 .then(() => {
+                    let successHtml = (navigator.language === 'ko')
+                        ? ('<b>' + studyName + '</b> 삭제가 완료되었습니다')
+                        : ('Deletion of <b>' + studyName + '</b> is complete');
+
                     ToastResponse.fire({
-                        html: '<b>[' + studyName + ']</b> 삭제가 완료되었습니다',
+                        html: successHtml,
                         icon: 'success'
                     }).then(() => {
                         location.href = '/';
@@ -123,11 +171,27 @@ function deleteStudy(studyName, studyId) {
                 })
                 .catch((error) => {
                     let jsonData = error.response.data;
+
+                    let errorText;
+                    if (jsonData['message'] === '로그인이 필요합니다') {
+                        errorText = (navigator.language === 'ko')
+                            ? (jsonData['message'])
+                            : ('Login is required');
+                    } else if (jsonData['message'] === '스터디 삭제는 스터디 리더만 가능합니다') {
+                        errorText = (navigator.language === 'ko')
+                            ? (jsonData['message'])
+                            : ('Study deletion can only be done by the study leader');
+                    } else {
+                        errorText = (navigator.language === 'ko')
+                            ? (jsonData['message'])
+                            : ('An error occurred on the server while sending the email');
+                    }
+
                     ToastResponse.fire({
                         color: '#FF0000',
-                        text: jsonData['message'],
+                        text: errorText,
                         icon: 'error'
-                    })
+                    });
                 });
         }
     });
