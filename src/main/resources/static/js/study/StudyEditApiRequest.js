05ada1335bbd27ad12ea05ebb060013ba01949df
SeoJiWon <sjiwon4491@gmail.com>
2022-11-21 20:19:25 +0900

    refactor: StudyEditApiRequest.js 국제화 메시지 설정 추가 (#45)

--- src/main/resources/static/js/study/StudyEditApiRequest.js
+++ src/main/resources/static/js/study/StudyEditApiRequest.js
@@ -10,8 +10,12 @@ function studyNameDuplicateCheckInEdit(studyId) {
     let studyNameVerificationToken = $('#studyNameVerificationToken');
 
     if (studyName.val().trim() === '') {
+        let nameValidationFailHtml = (navigator.language === 'ko')
+            ? ('<b>스터디 이름을 다시 확인해주세요</b><br><small>- 빈 값입니다</small>')
+            : ('<b>Please check the study name again</b><br><small>- Empty value exists</small>');
+
         ToastResponse.fire({
-            html: '<b>스터디 이름을 다시 확인해주세요</b><br><small>- 빈 값입니다</small>',
+            html: nameValidationFailHtml,
             icon: 'warning'
         }).then(() => {
             studyName.focus();
@@ -26,8 +30,12 @@ function studyNameDuplicateCheckInEdit(studyId) {
         focusConfirm: false
     });
 
+    let question = (navigator.language === 'ko')
+        ? ('해당 스터디명을 사용하시겠습니까?<br><b style="font-size: 20px;">-> ' + studyName.val() + '</b>')
+        : ('Would you like to use that study name?<br><b style="font-size: 20px;">-> ' + studyName.val() + '</b>');
+
     ToastApi.fire({
-        html: '해당 스터디명을 사용하시겠습니까?<br><b style="font-size: 20px;">-> ' + studyName.val() + '</b>',
+        html: question,
         icon: 'question'
     }).then((result) => {
         if (result.isConfirmed) {
@@ -38,8 +46,12 @@ function studyNameDuplicateCheckInEdit(studyId) {
 
             axios.post('/api/study/edit/duplicate-check', data)
                 .then(() => {
+                    let successHtml = (navigator.language === 'ko')
+                        ? ('<b>' + studyName.val() + '</b>은/는 사용 가능합니다')
+                        : ('<b>' + studyName.val() + '</b> is available');
+
                     ToastResponse.fire({
-                        html: '<b>[' + studyName.val() + ']</b>은/는 사용 가능합니다',
+                        html: successHtml,
                         icon: 'success'
                     }).then(() => {
                         studyNameDuplicateCheckButton.attr("disabled", true);
@@ -48,9 +60,14 @@ function studyNameDuplicateCheckInEdit(studyId) {
                 })
                 .catch((error) => {
                     let jsonData = error.response.data;
+
+                    let errorText = (navigator.language === 'ko')
+                        ? (jsonData['message'])
+                        : ('Duplicate study name');
+
                     ToastResponse.fire({
                         color: '#FF0000',
-                        text: jsonData['message'],
+                        text: errorText,
                         icon: 'error'
                     }).then(() => {
                         studyNameVerificationToken.val('fail');
