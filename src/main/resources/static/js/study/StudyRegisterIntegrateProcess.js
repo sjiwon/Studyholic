5b49fda6f9038073a0dc101a8fd68b089ae2d27f
SeoJiWon <sjiwon4491@gmail.com>
2022-11-21 20:35:26 +0900

    refactor: StudyRegisterIntegrateProcess.js 국제화 메시지 설정 추가 (#45)

--- src/main/resources/static/js/study/StudyRegisterIntegrateProcess.js
+++ src/main/resources/static/js/study/StudyRegisterIntegrateProcess.js
@@ -7,16 +7,24 @@ function studyRegister(userId, editor, tagify) {
     let studyName = $('#studyName'); // 스터디 이름
     let studyNameVerificationToken = $('#studyNameVerificationToken');
     if (validateionStudyName(studyName, studyNameVerificationToken) === 'fail1') {
+        let nameValidationFailHtml1 = (navigator.language === 'ko')
+            ? ('<b>스터디 이름을 다시 확인해주세요</b><br><small>- 빈 값입니다</small>')
+            : ('<b>Please check the study name again</b><br><small>- Empty value exists</small>');
+
         ToastResponse.fire({
-            html: '<b>스터디명을 다시 확인해주세요</b><br><small>- 빈 값입니다</small>',
+            html: nameValidationFailHtml1,
             icon: 'warning'
         }).then(() => {
             studyName.focus();
         })
         return false;
     } else if (validateionStudyName(studyName, studyNameVerificationToken) === 'fail2') {
+        let nameValidationFailHtml2 = (navigator.language === 'ko')
+            ? ('<b>스터디명 중복 체크를 진행해주세요</b>')
+            : ('<b>Please proceed with the duplicate study name check</b>');
+
         ToastResponse.fire({
-            html: '<b>스터디명 중복 체크를 진행해주세요</b>',
+            html: nameValidationFailHtml2,
             icon: 'warning'
         })
         return false;
@@ -24,8 +32,12 @@ function studyRegister(userId, editor, tagify) {
 
     let studyBriefDescription = $('#studyBriefDescription'); // 스터디 간단 설명
     if (validationStudyBriefDescription(studyBriefDescription) === false) {
+        let briefDescriptionValidationFailHtml = (navigator.language === 'ko')
+            ? ('<b>스터디 간단 설명을 다시 확인해주세요</b><br><small>- 빈 값입니다</small>')
+            : ('<b>Please check the study brief description again</b><br><small>- Empty value exists</small>');
+
         ToastResponse.fire({
-            html: '<b>스터디 간단 설명을 다시 확인해주세요</b><br><small>- 빈 값입니다</small>',
+            html: briefDescriptionValidationFailHtml,
             icon: 'warning'
         }).then(() => {
             studyBriefDescription.focus();
@@ -36,8 +48,12 @@ function studyRegister(userId, editor, tagify) {
     let studyDescription = editor.getHTML(); // 스터디 상세 설명
     let studyMaxMember = $('#studyMaxMember'); // 스터디 모집 회원 수
     if (validationStudyMaxMember(studyMaxMember)) {
+        let maxMemberValidationFailHtml = (navigator.language === 'ko')
+            ? ('<b>스터디 최대 인원수를 다시 확인해주세요</b><br><small>- 최소 2명, 최대 10명까지 가능합니다</small>')
+            : ('<b>Please double check the maximum number of study participants.</b><br><small>- Minimum of 2 people, maximum of 10 people</small>');
+
         ToastResponse.fire({
-            html: '<b>스터디 최대 인원수를 다시 확인해주세요</b><br><small>- 최소 2명, 최대 10명까지 가능합니다</small>',
+            html: maxMemberValidationFailHtml,
             icon: 'warning'
         })
         return false;
@@ -58,8 +74,12 @@ function studyRegister(userId, editor, tagify) {
         focusConfirm: false
     });
 
+    let question = (navigator.language === 'ko')
+        ? ('<b>' + studyName.val() + '</b>을 등록하시겠습니까?')
+        : ('Would you like to register for the <b>' + studyName.val() + '</b>');
+
     ToastApi.fire({
-        html: '<b>' + studyName.val() + '</b>을 등록하시겠습니까?',
+        html: question,
         icon: 'question'
     }).then((result) => {
         if (result.isConfirmed) {
@@ -75,8 +95,12 @@ function studyRegister(userId, editor, tagify) {
 
             axios.post('/api/study', data)
                 .then(() => {
+                    let successText = (navigator.language === 'ko')
+                        ? ('스터디 등록이 완료되었습니다')
+                        : ('Study registration is complete');
+
                     ToastResponse.fire({
-                        text: '스터디 등록이 완료되었습니다',
+                        text: successText,
                         icon: 'success'
                     }).then(() => {
                         location.href = '/user/' + userId + '/study';
@@ -84,9 +108,21 @@ function studyRegister(userId, editor, tagify) {
                 })
                 .catch(error => {
                     let jsonData = error.response.data;
+
+                    let errorText;
+                    if (jsonData['message'] === '사용자 정보가 존재하지 않습니다') {
+                        errorText = (navigator.language === 'ko')
+                            ? (jsonData['message'])
+                            : ('User information does not exist');
+                    } else {
+                        errorText = (navigator.language === 'ko')
+                            ? (jsonData['message'])
+                            : ('An error occurred on the server while sending the email');
+                    }
+
                     ToastResponse.fire({
                         color: '#FF0000',
-                        text: jsonData['message'],
+                        text: errorText,
                         icon: 'error'
                     })
                 });
